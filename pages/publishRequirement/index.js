0cb53c05739e18b9c5cfc685727d5fbdf48fff40
HeXiaPeng <2414488259@qq.com>
2023-08-27 21:24:26 +0800

    发布需求页面

--- pages/publishRequirement/index.js
+++ pages/publishRequirement/index.js
@@ -11,13 +11,23 @@ Component({
    * 组件的初始数据
    */
   data: {
-
+    reqTypeList: ['设计服务', '修改bug', '新需求'],
+    reqTypeIndex: 0
   },
 
+  
   /**
    * 组件的方法列表
    */
   methods: {
-
+    bindPickerChange: function(e) {
+      console.log('picker发送选择改变，携带s值为', e.detail.value)
+      this.setData({
+        reqTypeIndex: e.detail.value
+      })
+    },
+    formSubmit: function(e) {
+      console.log('form发生了submit事件，携带数据为：', e.detail.value)
+    },
   }
 })
